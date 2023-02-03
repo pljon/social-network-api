15e26f2c719622aa479ba5db9565855752e260b9
Jonathan Plaras <jonplaras@gmail.com>
2023-02-03 01:16:10 -0500

    routes and controllers file structure added

--- models/User.js
+++ models/User.js
@@ -1,42 +1,48 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
     },
     thoughts: [
-        {
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Thought'
-    }
-],
+        ref: "Thought",
+      },
+    ],
     friends: [
-        {
+      {
         type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
-},
-{
+        ref: "User",
+      },
+    ],
+  },
+  {
     toJSON: {
-        virtuals: true,
+      virtuals: true,
     },
-    id: false
-});
+    id: false,
+  }
+);
 
 // get total count of friends on retrieval
-UserSchema.virtual('friendCount').get(function() {
-    return `${this.friends.length}`;
+UserSchema.virtual("friendCount").get(function () {
+  return `${this.friends.length}`;
 });
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
