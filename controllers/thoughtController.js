77892a559613e9535ae95c8d7188be3c62b86ad9
Jonathan Plaras <jonplaras@gmail.com>
2023-02-03 18:14:15 -0500

    completed CRUD for thoughts, needs testing

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,3 @@
-const { model } = require("mongoose");
 const { Thought, User } = require("../models");
 
 module.exports = {
@@ -40,15 +39,59 @@ module.exports = {
           // find user by id and update thoughts array with new thought id
           { _id: req.body.userId }, // find user by id in body object
           { $push: { thoughts: _id } }, // push new thought id to thoughts array
-          { new: true } // return updated user / document as new object instead of original object
+          { new: true } // return new document
         );
       })
-      // then send updated user as json
-      .then((thought) => 
-        !thought // if no thought is found
-          ? res.status(404).json({ message: "No thought found with this id!" }) // send 404 error
-          : res.json(thought) // if thought, send thought
+      // then send thought as json, now with user data
+      .then(
+        (thought) =>
+          !thought // if no thought is found
+            ? res
+                .status(404)
+                .json({ message: "No thought found with this id!" }) // send 404 error
+            : res.json(thought) // respond with thought data
       )
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // update thought
+  updateThought(req, res) {
+    // find thought by id in params object and update with data in body object
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId }, // find thought by id in params object
+      { $set: req.body }, // set thought data to data in body object
+      { runValidators: true, new: true } // run validators and return new document
+    )
+      .then(
+        (thought) =>
+          !thought // if no thought is found
+            ? res
+                .status(404)
+                .json({ message: "No thought found with this id!" }) // send 404 error
+            : res.json(thought) // respond with thought data
+      )
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // delete thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId }) // find thought by id in params object
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No thought found with this id!" })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId }, // find user by thought id in thoughts array
+              { $pull: { thoughts: req.params.thoughtId } }, // pull thought id from thoughts array in user
+              { new: true } // respond with new document
+            )
+      )
+      .then((user) => {
+        !user
+          ? res.status(404).json({
+              message: "No user found associated with this thought id!",
+            })
+          : res.json(user); // respond with user data with thought removed from thoughts array
+      })
       .catch((err) => res.status(400).json(err));
   },
 };
