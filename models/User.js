f8c3c5c036584628c69efd3f4256151582cccdad
Jonathan Plaras <jonplaras@gmail.com>
2023-02-03 00:56:08 -0500

    created user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,37 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    thoughts: {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    },
+    friends: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+});
+
+// get total count of friends on retrieval
+userSchema.virtual('friendCount').get(function() {
+    return `${this.friends.length}`;
+});
+
+module.exports = User;
