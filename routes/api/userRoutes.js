131c8101fec05dd891d52e97f141be5c8bcfa6a5
Jonathan Plaras <jonplaras@gmail.com>
2023-02-03 01:30:08 -0500

    created userroute structure

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+
+const {
+  getUser,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/userController.js");
+
+// /api/users
+router.route("/").get(getUser).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
